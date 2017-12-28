486f14fc08b9c1b4b36f518394077e1537609d2b
cheenwe <cheenwe@gmail.com>
2017-12-28 10:41:57 +0800

    fixed emoji url

--- editormd.js
+++ editormd.js
@@ -3365,7 +3365,7 @@
 
     // Emoji graphics files url path
     editormd.emoji     = {
-        path  : "http://www.emoji-cheat-sheet.com/graphics/emojis/",
+        path  : "https://www.webpagefx.com/tools/emoji-cheat-sheet/graphics/emojis/",
         ext   : ".png"
     };
 
